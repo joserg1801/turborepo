b7dac3dda4bb31a6ee021a85c965b66c67ee26fc
josem1801 <josemanuelrosales2@gmail.com>
2023-02-12 23:52:04 -0600

    config nextjs to build on amplify

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,11 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  output: "standalone",
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 };
